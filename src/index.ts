7cba021e379508223d8aaa5124636ac3c8612675
mariatonicher <mariatonicher@users.noreply.github.com>
2023-12-11 22:03:05 +0000

    Buscar página html e renderizar no elemento root #1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const renderPage = async (pageName: string): Promise<void> => {
+  const html = await fetch(`pages/${pageName}.html`).then((resp) =>
+    resp.text()
+  );
+
+  const rootElem = document.getElementById("root") as HTMLElement;
+  rootElem.innerHTML = html;
+};
+
+renderPage("start"); // trocar pagina
+
+/* fetch("pages/start.html")
+  .then((resp) => resp.text())
+  .then((html) => {
+    const rootElem = document.getElementById("root") as HTMLElement;
+    rootElem.innerHTML = html;
+  }); //npm run dev, fetch com as 3 paginas
+ */
